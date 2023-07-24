2ac936b3bf29a0ded6372a53750c730f32da472d
christopherku777 <114657926+christopherku777@users.noreply.github.com>
2023-07-24 16:38:13 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,29 +1,38 @@
 // Function to display the popup
 function showPopup() {
-  const popup = document.getElementById('popup-content');
+  const popup = document.getElementById('popup');
   popup.style.display = 'block';
 }
 
 // Function to hide the popup
 function hidePopup() {
-  const popup = document.getElementById('popup-content');
+  const popup = document.getElementById('popup');
   popup.style.display = 'none';
 }
 
-// Schedule the alarm to show the popup every 30 minutes
-chrome.alarms.create({ delayInMinutes: 30, periodInMinutes: 30 });
+// Function to get the selected time increment from Chrome storage
+function getTimeIncrement(callback) {
+  chrome.storage.sync.get('timeIncrement', (data) => {
+    const timeIncrement = parseInt(data.timeIncrement, 10) || 5; // Default to 5 minutes if not set
+    callback(timeIncrement);
+  });
+}
+
+// Schedule the alarm to show the popup based on the user's selected time increment
+function scheduleAlarm() {
+  getTimeIncrement((timeIncrement) => {
+    chrome.alarms.create('stretchReminder', { periodInMinutes: timeIncrement });
+  });
+}
 
 // Handle the alarm event
-chrome.alarms.onAlarm.addListener(alarm => {
+chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === 'stretchReminder') {
     showPopup();
+    setTimeout(hidePopup, 5000); // Hide the popup after 5 seconds (adjust the value as needed)
   }
 });
 
-// Hide the popup when clicked
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('popup-content').addEventListener('click', hidePopup);
+  scheduleAlarm();
 });
-
-// Hide the popup after a few seconds (adjust the value as needed)
-setTimeout(hidePopup, 5000);
